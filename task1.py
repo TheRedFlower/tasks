abf7f1d77b124db9bff5f30aa1d10e9d98908b06
TheRedFlower <103130476+TheRedFlower@users.noreply.github.com>
2022-04-06 19:26:37 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+import math
+a = int(input('a: '))
+b = int(input('b: '))
+i = a**2 + b**2
+c = math.sqrt(i)
+print(c)
---
+++ task2.py
@@ -0,0 +1,4 @@
+import math
+r = int(input('r: '))
+s = 3.14 * (r**2)
+print('r:', s)
